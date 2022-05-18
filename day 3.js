8dee427aa1e0423dc4702f6e68c7a86c729cb720
nadaosamasayed <101336309+nadaosamasayed@users.noreply.github.com>
2022-05-19 00:09:18 +0200

    Add files via upload

--- day 3.js
+++ day 3.js
@@ -1,4 +1,4 @@
- var fruits =  ["apple", "strawberry", "banana", "orange", "mango"] 
+var fruits =  ["apple", "strawberry", "banana", "orange", "mango"] 
 //1-a
 var nada =fruits.findIndex(function(nada)
 {
@@ -10,7 +10,7 @@ var nada =fruits.findIndex(function(nada)
 
 
 )
-//1-b
+// //1-b
  function check(Fruit) {
         for(var i=0; i<Fruit.length; i++){
           if(typeof Fruit[i] != "string") {
@@ -21,7 +21,7 @@ var nada =fruits.findIndex(function(nada)
         return true;
        }
        console.log(check(fruits));
-//1-c
+// //1-c
 
 var fr1 = "apple" 
 console.log(fr1.startsWith("a"))
@@ -39,7 +39,7 @@ let fruitsFilter= fruits.filter(function(item,index)
 })
 console.log(fruitsFilter);
 
-//1-e
+// //1-e
 
 let fruitsFilter2= fruits.map(function nada(index,item)
 {
@@ -60,4 +60,45 @@ fruits.forEach(function nada(item,index){
 
 
 //2-a
+
+function text(){
+    let ID = document.getElementById("fruits").value;
+  
+    fetch("https://jsonplaceholder.typicode.com/posts?id-" + ID).then(function(data){
+
+
+data.json().then(function(tex){
+   console.log(tex)
+
+})
+
+
+    }).catch(function(tex2){
+        console.log(tex2)
+    })
+}
+
+//2-b
+
+let h2 = document.getElementById("h2");
+let h3 = document.getElementById("h3")
+let h4 = document.getElementById("h4")
+
+async function products(){
+    let response = await fetch("https://60523dc8fb49dc00175b7d04.mockapi.io/api/v1/products")
+let data = await response.json()
+console.log(data)
+for(let i=0;i<data.length;i++)
+{
+    h2.innerHTML = data[i].name;
+    h3.innerHTML = data[i].price;
+    h4.innerHTML = data[i].image;
+
+}
+
+
+
+}
+
+products()
 
